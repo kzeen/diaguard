e6a700b186d52a2545e0aff9838fe2e6873e1819
zeen <157370421+kzeen@users.noreply.github.com>
2025-04-17 14:47:15 +0300

    Register prediction models for admin

--- backend/predictions/admin.py
+++ backend/predictions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import HealthInput, Prediction, Explanation, Recommendation
 
-# Register your models here.
+admin.site.register(HealthInput)
+admin.site.register(Prediction)
+admin.site.register(Explanation)
+admin.site.register(Recommendation)
